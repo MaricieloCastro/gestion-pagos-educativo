33e8a1560f0595203fceb3f810ab8af9f15d16e6
IsmaelHaroC <166766644+IsmaelHaroC@users.noreply.github.com>
2024-04-16 19:14:01 -0500

    cambios para merge

--- src/modules/Seguridad/pages/RestablecerContrasenia/RestablecerContrasenia.jsx
+++ src/modules/Seguridad/pages/RestablecerContrasenia/RestablecerContrasenia.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
-
 const RestablecerContrasenia = () => {
     return (
-        <div>RestablecerContrasenia</div>
-    )
-}
+      <div className="h-screen grid grid-cols-login-grid">
+        <div className="">
+Restablecesr
+        </div>
+        <div className="login__img" />
+      </div>
+    );
+  };
 
 export default RestablecerContrasenia
