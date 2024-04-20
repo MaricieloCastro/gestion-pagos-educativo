6d2d31b24c225e0b74da119432bc4b79708478fb
Manuel Gómez <132093205+weonfredo@users.noreply.github.com>
2024-04-20 17:10:59 -0500

    agregar componente modales, toast y prueba wilmar, y un url para las pruebas

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,10 @@ import { Routes, Route } from "react-router-dom";
 //Modulo de seguridad
 import InformacionUsuario from "./modules/Seguridad/pages/InformacionUsuario";
 
+//Pruebas
+import PruebasModales from "../src/modules/Seguridad/pages/PruebasModales";
+import PruebasToast from "../src/modules/Seguridad/pages/PruebasToast";
+
 import IniciarSesion from "./modules/Seguridad/pages/IniciarSesion";
 import ListaUsuarios from "./modules/Seguridad/pages/ListaUsuarios";
 import PanelAdministrador from "./modules/Seguridad/pages/PanelAdministrador";
@@ -26,13 +30,30 @@ const App = () => {
   return (
     <div className="bg-blue-claro h-screen">
       <Routes>
-        <Route element={<IniciarSesion />} path={enlaces[0].prevPath + enlaces[0].path} />
-        <Route element={<RestablecerContrasenia />} path={enlaces[1].prevPath + enlaces[1].path} />
-        <Route element={<ActualizarContrasenia />} path={enlaces[2].prevPath + enlaces[2].path} />
+        <Route element={<PruebasModales />} path="/Pruebas/Modal" />
+        <Route element={<PruebasToast />} path="/Pruebas/toast" />
+        <Route
+          element={<IniciarSesion />}
+          path={enlaces[0].prevPath + enlaces[0].path}
+        />
+        <Route
+          element={<RestablecerContrasenia />}
+          path={enlaces[1].prevPath + enlaces[1].path}
+        />
+        <Route
+          element={<ActualizarContrasenia />}
+          path={enlaces[2].prevPath + enlaces[2].path}
+        />
         {/* <Route element={<PrivateRoutes />}> */}
         <Route element={<MenuPrincipal />} path={enlaces[3].path} />
-        <Route element={<Perfil />} path={enlaces[4].prevPath + enlaces[4].path} />
-        <Route element={<PanelAdministrador />} path={enlaces[5].prevPath + enlaces[5].path} />
+        <Route
+          element={<Perfil />}
+          path={enlaces[4].prevPath + enlaces[4].path}
+        />
+        <Route
+          element={<PanelAdministrador />}
+          path={enlaces[5].prevPath + enlaces[5].path}
+        />
         <Route
           element={<ListaUsuarios />}
           path={enlaces[6].prevPath + enlaces[6].path}
