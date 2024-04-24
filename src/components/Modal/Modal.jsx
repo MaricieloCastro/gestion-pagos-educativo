6f3f9eeade2edcf8e6c93b1189e6620688522c32
Manuel Gómez <132093205+weonfredo@users.noreply.github.com>
2024-04-23 20:31:21 -0500

    subiendo toast and modals

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,40 +1,41 @@
-import React, { useState } from 'react';
-import { Button, Modal } from 'antd';
+import React, { useState } from "react";
+import { Button, Modal } from "antd";
 
 const ModalAnt = (props) => {
+  const { setOpen, open } = props;
 
-    const { setOpen, open } = props
-
-    const [confirmLoading, setConfirmLoading] = useState(false);
-    const [modalText, setModalText] = useState('SAJDASJDJASDJASDJSDMASJCZJXC ZJXC ZXJC ZJXCZXCMASJCAMJSCASMCAJSCMAJSCMASJCASJ CJ CJ SACJSDMFASJDMASJDASJDMASDA CJZXC ZJCZJXC ZJ DJASJDAASDASDASDASDASDADADASDASDASD');
-    const handleOk = () => {
-        setModalText('The modal will be closed after two seconds');
-        setConfirmLoading(true);
-        setTimeout(() => {
-            setOpen(false);
-            setConfirmLoading(false);
-        }, 2000);
-    };
-    const handleCancel = () => {
-        console.log('Clicked cancel button');
-        setOpen(false);
-    };
-    return (
-        <>
-            <Modal
-                title="Title"
-                centered
-                open={open}
-                onOk={handleOk}
-                confirmLoading={confirmLoading}
-                onCancel={handleCancel}
-                okText="ACEPTAR"
-                cancelText="CANCELAR"
-            // width={450}
-            >
-                <p>{modalText}</p>
-            </Modal>
-        </>
-    );
+  const [confirmLoading, setConfirmLoading] = useState(false);
+  const [modalText, setModalText] = useState(
+    "SAJDASJDJASDJASDJSDMASJCZJXC ZJXC ZXJC ZJXCZXCMASJCAMJSCASMCAJSCMAJSCMASJCASJ CJ CJ SACJSDMFASJDMASJDASJDMASDA CJZXC ZJCZJXC ZJ DJASJDAASDASDASDASDASDADADASDASDASD"
+  );
+  const handleOk = () => {
+    setModalText("The modal will be closed after two seconds");
+    setConfirmLoading(true);
+    setTimeout(() => {
+      setOpen(false);
+      setConfirmLoading(false);
+    }, 2000);
+  };
+  const handleCancel = () => {
+    console.log("Clicked cancel button");
+    setOpen(false);
+  };
+  return (
+    <>
+      <Modal
+        title="Title"
+        centered
+        open={open}
+        onOk={handleOk}
+        confirmLoading={confirmLoading}
+        onCancel={handleCancel}
+        okText="ACEPTAR"
+        cancelText="CANCELAR"
+        // width={450}
+      >
+        <p>{modalText}</p>
+      </Modal>
+    </>
+  );
 };
 export default ModalAnt;
